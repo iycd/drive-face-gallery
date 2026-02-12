aed82b1816fcb8310f42505683ca996d53817ca5
iycd <romlihuna@gmail.com>
2026-02-12 09:45:02 +0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true // Memungkinkan akses dari network lokal jika diperlukan
+  }
+})
