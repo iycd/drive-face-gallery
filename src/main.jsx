3cbc035db53b930a75df7c3b7fef2c7877f00fb1
iycd <romlihuna@gmail.com>
2026-02-12 19:44:10 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css' // Uncomment jika Anda sudah membuat file CSS
 
+// Kode ini mencari elemen dengan ID 'root' di index.html
+// Lalu merender komponen <App /> di dalamnya.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
